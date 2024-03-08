b7316b2404dff94be201560984a522bf463faf19
serdarkayaci <serdar.kayaci@stu.pirireis.edu.tr>
2024-03-08 14:31:03 +0300

    xxx

---
+++ myCV/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from .views import index
+from .views import contact
+
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('contact/', contact, name='contact'),
+]
