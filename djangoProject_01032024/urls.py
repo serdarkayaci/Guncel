3f06008ad8c638faac45242a37bfe212c055ea69
Serdar Kayaci <161631488+serdarkayaci@users.noreply.github.com>
2024-03-15 12:53:37 +0300

    YENİ

--- djangoProject_01032024/urls.py
+++ djangoProject_01032024/urls.py
@@ -1,7 +1,7 @@
 """
 URL configuration for djangoProject_01032024 project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
